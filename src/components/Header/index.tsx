d552e8b06531134543b55f5f6a13196113162747
yonlu <lss.emailbox@gmail.com>
2022-02-24 14:04:34 -0500

    feat: add header and corresponding styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <Image src="/logo.svg" alt="logo" width="238" height="25" />
+        </a>
+      </Link>
+    </header>
+  );
 }
